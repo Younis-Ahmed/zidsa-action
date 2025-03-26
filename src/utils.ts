c0683a8850df0216ae984a81d76689882b4e2110
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-03-27 02:47:12 +0300

    refactor: simplify getVariables function and add getWorkspacePath

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,11 @@
-import * as core from '@actions/core'
-
+import { getInput } from '@actions/core'
 
 export type VariableKey = 'EMAIL' | 'PASSWORD' | 'THEME_ID'
 
 export interface Variables {
-    EMAIL: string;
-    PASSWORD: string;
-    THEME_ID: string;
+  EMAIL: string
+  PASSWORD: string
+  THEME_ID: string
 }
 
 /**
@@ -14,9 +13,22 @@ export interface Variables {
  * @param variables Array of variable keys to retrieve
  * @returns Object containing the requested variables and their values
  */
-export function getVariables<T extends VariableKey>(variables: T[]): Pick<Variables, T> {
-    return variables.reduce((acc, variable) => {
-        acc[variable] = core.getInput(variable);
-        return acc;
-    }, {} as Pick<Variables, T>);
+export function getVariables<T extends VariableKey>(
+  variables: T[]
+): Pick<Variables, T> {
+  return variables.reduce(
+    (acc, variable) => {
+      acc[variable] = getInput(variable)
+      return acc
+    },
+    {} as Pick<Variables, T>
+  )
+}
+
+/**
+ * Gets the workspace directory path from checkout-out files
+ * @returns The path to the workspace directory
+ */
+export function getWorkspacePath(): string {
+  return process.env.GITHUB_WORKSPACE || '.'
 }
