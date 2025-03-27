1bc5ed9d0430a776905376cac0fe012cb9366448
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-03-27 21:11:26 +0300

    style: improve import formatting in getVersionBump.ts

--- src/getVersionBump.ts
+++ src/getVersionBump.ts
@@ -1,10 +1,10 @@
+import type { BumperRecommendation } from 'conventional-recommended-bump'
 import {
   Bumper,
-  type BumperRecommendation
 } from 'conventional-recommended-bump'
 
 export async function getVersionBump(
-  build_path: string
+  build_path: string,
 ): Promise<BumperRecommendation> {
   const bumper = new Bumper(build_path).loadPreset('angular')
   const recommendation = await bumper.bump()
