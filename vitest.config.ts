43d6388d16efe235eb3326d8f2ddda3d6d3e8954
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-03-28 02:34:38 +0300

    build: add JSON plugin to Rollup config and update output directory

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,32 +1,32 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    test: {
-        environment: 'node',
-        include: ['**/*.test.ts'],
-        exclude: ['/dist/', '/node_modules/'],
-        clearMocks: true, // Vitest automatically resets mocks, so this might be redundant
-        reporters: ['verbose'],
-        coverage: {
-            enabled: true,
-            provider: 'istanbul', // or 'v8'
-            reporter: ['json-summary', 'text', 'lcov'],
-            reportsDirectory: './coverage',
-            include: ['./src/**'],
-            exclude: ['/node_modules/', '/dist/'],
-            thresholds: {
-                global: {
-                    branches: 100,
-                    functions: 100,
-                    lines: 100,
-                    statements: 100,
-                },
-            },
+  test: {
+    environment: 'node',
+    include: ['**/*.test.ts'],
+    exclude: ['/dist/', '/node_modules/'],
+    clearMocks: true, // Vitest automatically resets mocks, so this might be redundant
+    reporters: ['verbose'],
+    coverage: {
+      enabled: true,
+      provider: 'istanbul', // or 'v8'
+      reporter: ['json-summary', 'text', 'lcov'],
+      reportsDirectory: './coverage',
+      include: ['./src/**'],
+      exclude: ['/node_modules/', '/dist/'],
+      thresholds: {
+        global: {
+          branches: 100,
+          functions: 100,
+          lines: 100,
+          statements: 100,
         },
+      },
     },
-    esbuild: {
-        jsxFactory: 'h',
-        jsxFragment: 'Fragment',
-        loader: 'ts',
-    },
-});
+  },
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    loader: 'ts',
+  },
+})
