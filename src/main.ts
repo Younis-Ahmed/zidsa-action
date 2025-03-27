9b006285aeaff3c4770c06287004c58bd6a6a366
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-03-27 03:27:20 +0300

    chore: remove ESLint configuration files and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { getVariables, type Variables } from './utils.js'
+import { getVariables, getWorkspacePath } from './utils.js'
+import { login } from './login.js'
 
 /**
  * The main function for the action.
@@ -7,11 +8,12 @@ import { getVariables, type Variables } from './utils.js'
  * @returns Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-    try {
-      const variables: Variables = getVariables(['EMAIL', 'PASSWORD', 'THEME_ID']);
-
-    } catch (error) {
-      // Fail the workflow run if an error occurs
-      if (error instanceof Error) core.setFailed(error.message)
-    }
+  try {
+    const variables = getVariables(['EMAIL', 'PASSWORD', 'THEME_ID'])
+    const workspacePath = getWorkspacePath()
+    login(variables.EMAIL, variables.PASSWORD)
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
   }
+}
