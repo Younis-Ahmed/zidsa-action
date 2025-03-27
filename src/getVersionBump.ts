1ec7647d3b5f99fc1aa7a07949ef488503fd5a29
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-03-27 03:23:53 +0300

    chore: remove Prettier and ESLint configurations, add version bump utility

---
+++ src/getVersionBump.ts
@@ -0,0 +1,13 @@
+import {
+  Bumper,
+  type BumperRecommendation
+} from 'conventional-recommended-bump'
+
+export async function getVersionBump(
+  build_path: string
+): Promise<BumperRecommendation> {
+  const bumper = new Bumper(build_path).loadPreset('angular')
+  const recommendation = await bumper.bump()
+
+  return recommendation
+}
